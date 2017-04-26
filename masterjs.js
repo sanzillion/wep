5d135755cd45d1fa8038bf3c96ac6a34fd3f74a1
JustFab Voice <JustFab Voice>
2017-04-26 21:17:41 +0800

    API working

--- masterjs.js
+++ masterjs.js
@@ -1,80 +1,43 @@
-console.log("Fuckers");
+
+
+var lat;
+var long;
+var url;
+var data;
+
 $(document).ready(function(){
-	console.log("Inside jqeury");
-});
+    getLocation();
 
-/*
-  Code by Gabriel Nunes
-*/
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(locations);
+        } else {
+           console.log("Geolocation is not supported by this browser.");
+        }
+    }
 
-function inIframe () { try { return window.self !== window.top; } catch (e) { return true; } }
+    function locations(loc){
+            lat = loc.coords.latitude;
+            long = loc.coords.longitude;
+          url = 'http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long+'&APPID=3602ffd43c9f394519c6b566a17005a1&units=metric';
+          requests(url);
+    }
 
-var colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#736466", "#28B8AB", "#47749B", "#64A541"];
-var currentQuote = '', currentAuthor = '';
-
-function openURL(url){
-  window.open(url, 'Share', 'width=550, height=400, toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0');
-}
-
-function getQuote() {
-  $.ajax({
-    headers: {
-      "X-Mashape-Key": "OivH71yd3tmshl9YKzFH7BTzBVRQp1RaKLajsnafgL2aPsfP9V",
-      Accept: "application/json",
-      "Content-Type": "application/x-www-form-urlencoded"
-    },
-    url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=',
-    success: function(response) {
-      var r = JSON.parse(response);
-      console.log("Whatever");
-      console.log(r);
-      currentQuote = r.quote;
-      currentAuthor = r.author;
-      if(inIframe())
-      {
-        $('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
+    function requests(url){
+      if(url){
+        data = $.getJSON(url, function(){
+            console.log(data.statusText);
+        });
+          data.fail(function() {
+            console.log( "error" );
+          });
       }
-      $(".quote-text").animate({
-          opacity: "0",
-          right: "30px"
-        }, 500,
-        function() {
-          $(this).css("right", "-70px");
-          $(this).animate({
-            opacity: "1",
-            right: "0px"
-          }, 500);
-          $('#text').text(r.quote);
-        });
-
-      $(".quote-author").animate({
-          opacity: 0
-        }, 500,
-        function() {
-          $(this).animate({
-            opacity: 1
-          }, 500);
-          $('#author').html(r.author);
-        });
-
-      var color = Math.floor(Math.random() * colors.length);
-      $("html body").animate({
-        backgroundColor: colors[color],
-        color: colors[color]
-      }, 1000);
-      $(".button").animate({
-        backgroundColor: colors[color]
-      }, 1000);
     }
-  });
-}
 
-$(document).ready(function() {
-  getQuote();
-  $('#new-quote').on('click', getQuote);
-  $('#tweet-quote').on('click', function() {
-    if(!inIframe()) {
-      openURL('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
+    function showData(data){
+      //$('.text').text(data);
     }
-  });
+    
 });
+
+
