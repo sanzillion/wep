675a1e9e1f992e20097a7d3092168d8a1706ff8d
JustFab Voice <JustFab Voice>
2017-04-28 22:52:05 +0800

    added animated icons

--- masterjs.js
+++ masterjs.js
@@ -1,4 +1,7 @@
 
+var shit = "https://codepen.io/joshbader/pen/EjXgqr?editors=0100";
+var test = "https://openweathermap.org/weather-conditions";
+
 var lat;
 var long;
 var url;
@@ -6,6 +9,18 @@ var data;
 var data2;
 var metric;
 var imperial;
+
+var wicon = {
+  sunshower : "<div class='icon sun-shower'><div class='cloud'></div><div class='sun'><div class='rays'></div></div><div class='rain'></div></div>",
+  thunderstorm: '<div class="icon thunder-storm"><div class="cloud"></div><div class="lightning"><div class="bolt"></div><div class="bolt"></div></div></div>',
+  cloudy: '<div class="icon cloudy"><div class="cloud"></div><div class="cloud"></div></div>',
+  flurries: '<div class="icon flurries"><div class="cloud"></div><div class="snow"><div class="flake"></div><div class="flake"></div></div></div>',
+  sunny: '<div class="icon sunny"><div class="sun"><div class="rays"></div></div></div>',
+  rainy: '<div class="icon rainy"><div class="cloud"></div><div class="rain"></div></div>'
+};
+
+var wi;
+
 
 $(document).ready(function(){
     getLocation();
@@ -49,9 +64,9 @@ $(document).ready(function(){
       console.log("Imperial Temp: "+imperial.main.temp);
       console.log("Metric Temp: "+metric.main.temp);
 
-        var weather = metric.weather[0].main;
+        var weather = metric.weather[0].main.toLowerCase();
         var icon = "http://openweathermap.org/img/w/";
-        var time = icon.substring(3,2);
+        time = icon.substring(3,2);
         var icon = icon + metric.weather[0].icon + ".png";
         var index = 0;
         var img;
@@ -64,44 +79,74 @@ $(document).ready(function(){
 
         var add = "img/";
         var images = {
-          "day": {
-            "normal": ['cloudy.jpg','sunrise.png','sunset.png','sunset.jpg'],
-            "rain": 'rainmorning.jpg'
+          day: {
+            normal: ['cloudy.jpg','sunrise.png','sunset.png','sunset.jpg'],
+            rain: 'rainmorning.jpg'
           },
-          "night": {
-            "normal": ['night.png','night.jpg','night2.jpg','rain.jpg',],
-            "rain": 'rain.jpg'
+          night: {
+            normal: ['night.png','night.jpg','night2.jpg'],
+            rain: 'rain.jpg'
           }
         };
+
         if(time == "day"){
-          if(weather == "rain" || weather == "shower rain" || weather == "thunderstorm"){
+          if(weather !== "rain" || weather !== "shower rain" || weather !== "thunderstorm"){
             index = Math.floor(Math.random() * images.day.normal.length);
-            img = images.day[index];
-          }else{
+            img = images.day.normal[index];
+          }
+          else{
             img = images.day.rain;
           }
           
         }
         else{
-          if(weather == "rain" || weather == "shower rain" || weather == "thunderstorm"){
+          if(weather !== "rain" || weather !== "shower rain" || weather !== "thunderstorm"){
             index = Math.floor(Math.random() * images.night.normal.length);
-            img = images.night[index];
-          }else{
+            img = images.night.normal[index];
+          }
+          else{
             img = images.night.rain;
           }
           
         }
-
         var link = add+img;
-        
         var body = $('body,html');
           body.css('background','url('+link+') no-repeat center center fixed');
           body.css('background-size','cover');
 
-        $('#image').attr("src", icon);
+        switch(weather){
+          case "clear sky": 
+            wi = wicon.sunny;
+            break;
+          case "few clouds": case "scattered clouds": case "broken clouds":
+            wi = wicon.cloudy;
+            break;
+          case "shower rain":
+            wi = wicon.rainy;
+            break;
+          case "rain":
+            if(time == "day"){
+              wi = wicon.sunshower;
+            }
+            else{
+              wi = wicon.rainy;
+            }
+            break;
+          case "thunderstorm":
+            wi = wicon.thunderstorm;
+            break;
+          case "snow":
+            wi = wicon.flurries;
+            break;
+          default:
+            wi = wicon.cloudy;
+            break;
+        }
+
+        $('#wicon').html(wi);
         $('#temp').text(metric.main.temp + " C");
         $('#place').text(metric.name + ", " + metric.sys.country);
-        $('#weather').text(weather + ": " + metric.weather[0].description);
+        $('#weathr').text(weather + ": " + metric.weather[0].description);
         $('#wind').text(metric.wind.speed + " knots");
     }
     
